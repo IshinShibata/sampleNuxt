f20b04a5e1708413bc41be0c2c3f89d61462443b
柴田維新 <kobukobudesu.dekaihou@gmail.com>
2020-05-03 18:59:24 +0900

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         sampleNuxt
       </h1>
       <h2 class="subtitle">
-        My laudable Nuxt.js project
+        My laudable なくすと project
       </h2>
       <div class="links">
         <a
